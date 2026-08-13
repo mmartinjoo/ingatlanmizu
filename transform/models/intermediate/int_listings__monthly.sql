fb646e0dbd816c7427a1a069847b8bb34adf4fb2
martinjoo <martin@martinjoo.dev>
2026-08-13 14:23:51 +0200

    Add listings monthly table

---
+++ transform/models/intermediate/int_listings__monthly.sql
@@ -0,0 +1,24 @@
+with listing_months as (
+    select distinct
+        listing_key,
+        date_trunc('month', observed_at)::date as month_start
+    from {{ ref('int_observations__unioned') }}
+)
+
+select distinct on (listing_months.listing_key, listing_months.month_start)
+    listing_months.month_start,
+    listing_months.listing_key,
+    versions.city,
+    versions.main_type,
+    versions.sub_type,
+    versions.price_huf,
+    versions.area_sqm
+from listing_months
+join {{ ref('int_listing_versions__unioned') }} as versions
+    on versions.listing_key = listing_months.listing_key
+    and versions.observed_at < listing_months.month_start + interval '1 month'
+order by
+    listing_months.listing_key,
+    listing_months.month_start,
+    versions.observed_at desc,
+    versions.bronze_id desc
