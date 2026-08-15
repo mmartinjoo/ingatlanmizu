20c3547cf18b37a0bc46bc306827b2e8a829c4df
martinjoo <martin@martinjoo.dev>
2026-08-15 10:22:03 +0200

    Clean up

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ rsync -avz \
   --exclude='.env.example' \
   --exclude='.git' \
   --exclude='transform/target/' \
+  --exclude='transform/logs/' \
   ./ alwyzon@203.34.137.201:/home/alwyzon/ingatlanmizu
 
 ssh -tt -o StrictHostKeyChecking=no alwyzon@203.34.137.201 "/home/alwyzon/ingatlanmizu/start_stack_on_remote.sh"
