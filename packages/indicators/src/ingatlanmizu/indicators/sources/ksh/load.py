eca822cea5e9dceb932d5ad4c9f8cac38db02b53
martinjoo <martin@martinjoo.dev>
2026-08-17 23:08:42 +0200

    Remove truncate

--- packages/indicators/src/ingatlanmizu/indicators/sources/ksh/load.py
+++ packages/indicators/src/ingatlanmizu/indicators/sources/ksh/load.py
@@ -2,10 +2,22 @@ from ingatlanmizu.core.db import connection
 
 def load(data: dict[str, float|None]):
     with connection() as conn:
-        conn.execute("truncate table bronze.ksh_inflation_values restart identity")
-        
         for month_start in data:
             if data[month_start] is None:
+                continue
+            
+            row = conn.execute("""
+                select exists (
+                    select 1
+                    from bronze.ksh_inflation_values
+                    where month_start = %s
+                    and inflation is not null
+                )                
+            """, (
+                month_start  
+            )).fetchone()
+            
+            if row[0]:
                 continue
             
             conn.execute("""
