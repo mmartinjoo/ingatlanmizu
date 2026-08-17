97e477395a9484df41ea0d99b67bdc9f52fcf02d
martinjoo <martin@martinjoo.dev>
2026-08-17 22:52:13 +0200

    ADd inflation to market indicators mart

--- transform/models/marts/mart_market_indicators_monthly.sql
+++ transform/models/marts/mart_market_indicators_monthly.sql
@@ -1,14 +1,12 @@
-with 
-	current_base_rate as (
-		select *
+select
+	date_trunc('month', available_at)::date as month_start,
+	(
+		select base_rate
 		from {{ source('bronze', 'mnb_base_rates') }}
 		order by valid_until desc
 		limit 1
-	)
-
-select
-	date_trunc('month', available_at)::date as month_start,
-	current_base_rate.base_rate,
+	) as base_rate,
+	inflation.inflation,
 	percentile_cont(0.5) within group(
 		order by apr
 	) as median_apr,
@@ -19,5 +17,7 @@ select
 	) as median_monthly_installment,
 	min(monthly_installment) as lowest_monthly_installment,
 	max(monthly_installment) as highest_monthly_installment
-from {{ source('bronze', 'bankmonitor_loans') }}, current_base_rate
-group by 1, 2
+from {{ source('bronze', 'bankmonitor_loans') }} as loans
+join {{ source('bronze', 'ksh_inflation_values') }} as inflation
+	on inflation.month_start = date_trunc('month', loans.available_at)::date - interval '1 month'
+group by 1, 2, 3
