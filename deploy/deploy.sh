585e2b45384ab64a54393a91020bfd0c687a5e76
martinjoo <martin@martinjoo.dev>
2026-09-10 09:36:15 +0200

    Fix deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,15 +1,17 @@
 set -e
 
 rsync -avz \
-  --exclude='.venv/' \
+  --exclude='api/.venv/' \
   --exclude='minio/' \
   --exclude='postgres/' \
   --exclude='.env' \
   --exclude='.env.example' \
   --exclude='.git' \
-  --exclude='transform/target/' \
-  --exclude='transform/logs/' \
-  --exclude='logs/' \
+  --exclude='.claude' \
+  --exclude='api/transform/target/' \
+  --exclude='api/transform/logs/' \
+  --exclude='frontend/node_modules/' \
+  --exclude='frontend/.vscode/' \
   ../ alwyzon@203.34.137.201:/home/alwyzon/ingatlanmizu
 
 ssh -tt -o StrictHostKeyChecking=no alwyzon@203.34.137.201 "cd /home/alwyzon/ingatlanmizu && docker compose -f docker-compose.yml -f docker-compose.prod.yml down"
